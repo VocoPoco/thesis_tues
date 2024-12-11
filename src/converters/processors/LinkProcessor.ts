2ae5039d55aafc359e2480dbde9478e6a703fc81
VocoPoco <vik.penev@gmail.com>
2024-12-11 13:15:02 +0200

    feat: added link processor

---
+++ src/converters/processors/LinkProcessor.ts
@@ -0,0 +1,16 @@
+import { Literal, Parent, RootContent } from 'mdast';
+import Processor from './Processor';
+
+class LinkProcessor extends Processor {
+  public constructProperties(node: RootContent): Record<string, string> {
+    return {
+      url: 'url' in node ? node.url || '' : '',
+      value:
+        'children' in node && 'value' in (node as Parent).children[0]
+          ? ((node as Parent).children[0] as Literal).value || ''
+          : '',
+    };
+  }
+}
+
+export default LinkProcessor;
