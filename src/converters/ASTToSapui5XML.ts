c3940565e6b59c8dcb2d506d22f8847cbde23556
VocoPoco <vik.penev@gmail.com>
2024-12-13 03:25:19 +0200

    feat: refactored logic traversal in main convertor file

--- src/converters/ASTToSapui5XML.ts
+++ src/converters/ASTToSapui5XML.ts
@@ -1,33 +1,39 @@
-// import { Root } from 'mdast';
-// import FileManager from '../utils/FileManager.js';
-// import Converter from './Converter.js';
+import { Parent, Root, RootContent } from 'mdast';
+import FileManager from '../utils/FileManager.js';
+import Converter from './Converter.js';
+import ProcessorFactory from './ProcessorFactory.js';
 
-// class ASTToSapui5XML extends Converter<Root, string> {
-//   private SAP_M_TAGS = {
-//     heading: '<Title level={depth}>{value}<Title>',
-//     text: '<Text text={value} class={type}>',
-//     list: '<List>',
-//     listItem: '<StandardListItem>',
-//     blockquote: "<FormattedText htmlText='{text}'/>",
-//     thematicBreak: "<ToolBar width='100%' height='1px'/>",
-//     code: "<code:CodeEditor editable='false' lineNumbers='false' language={lang} value={value}>",
-//     // TABLE: '<table:Table>',
-//     // THEAD: '<table:Column>',
-//     // TBODY: '<table:Row>',
-//     // TR: '<table:Row>',
-//     // TD: '<table:ColumnListItem>',
-//     // TH: "<table:Column header='Header'>",
-//   };
-//   public convert    () {}
-//   public export(
-//     content: string,
-//     dirname: string = 'src/resources',
-//     filename: string = 'sapui5XML',
-//     format: string = 'view.xml',
-//   ): void {
-//     const filePath: string = `${dirname}/${filename}.${format}`;
-//     FileManager.saveAsFile(filePath, content);
-//   }
-// }
+class ASTToSapui5XML extends Converter<Root, string> {
+  private convertChild(node: RootContent): string {
+    const processor = ProcessorFactory.getProcessor(node.type);
+    if (processor) {
+      return processor.processPlaceholders(node);
+    }
+    const children = 'children' in node ? (node as Parent).children || [] : [];
+    return children.map((child) => this.convertChild(child)).join('');
+  }
 
-// export default ASTToSapui5XML;
+  public convert(content: Root): string {
+    const queue: RootContent[] = [...content.children];
+    let result = '';
+
+    while (queue.length > 0) {
+      const child = queue.shift() as RootContent;
+      result += this.convertChild(child);
+    }
+
+    return result;
+  }
+
+  public export(
+    content: string,
+    dirname: string = 'src/resources',
+    filename: string = 'sapui5XML',
+    format: string = 'view.xml',
+  ): void {
+    const filePath: string = `${dirname}/${filename}.${format}`;
+    FileManager.saveAsFile(filePath, content);
+  }
+}
+
+export default ASTToSapui5XML;
