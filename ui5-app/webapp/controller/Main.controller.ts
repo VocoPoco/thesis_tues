3d2798fa66a160f1b5646c383f1f78e78bd6cedd
VocoPoco <vik.penev@gmail.com>
2025-01-07 15:15:31 +0200

    fix: removed unneeded files and started working on main controller dynamic loading

--- ui5-app/webapp/controller/Main.controller.ts
+++ ui5-app/webapp/controller/Main.controller.ts
@@ -1,11 +1,24 @@
-import MessageBox from "sap/m/MessageBox";
-import BaseController from "./BaseController";
+import XMLView from 'sap/ui/core/mvc/XMLView';
+import BaseController from './BaseController';
 
 /**
  * @namespace com.thesistues.ui5app.controller
  */
 export default class Main extends BaseController {
-	public sayHello(): void {
-		MessageBox.show("Hello World!");
-	}
+  public sayHello(): void {
+    this.loadDynamicView();
+  }
+  private async loadDynamicView(): Promise<void> {
+    try {
+      // Load the dynamic view
+      const dynamicView = await XMLView.create({
+        viewName: 'com.thesistues.ui5app.view.SapUI5',
+      });
+
+      const page = this.byId('page') as sap.m.Page;
+      page.addContent(dynamicView);
+    } catch (error) {
+      console.error('Error loading dynamic view:', error);
+    }
+  }
 }
