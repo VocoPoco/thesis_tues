d6e3f05f564b8051593c5f841003f524039f94cc
VocoPoco <vik.penev@gmail.com>
2025-01-16 13:19:53 +0200

    fix: forgot one file

---
+++ converter/src/converters/processors/type/Processor.ts
@@ -0,0 +1,29 @@
+import { RootContent } from 'mdast';
+import escapeSpecialCharacters from '../../../utils/ProcessorUtils.js';
+
+abstract class Processor {
+  constructor(protected template: string) {}
+
+  protected shouldEscape(): boolean {
+    return true;
+  }
+
+  protected abstract constructProperties(
+    node: RootContent,
+  ): Record<string, string>;
+
+  public processPlaceholders(node: RootContent): string {
+    const properties = this.constructProperties(node);
+
+    if ('value' in properties && this.shouldEscape()) {
+      properties.value = escapeSpecialCharacters(properties.value);
+    }
+
+    return Object.entries(properties).reduce(
+      (result, [key, value]) => result.replace(`{${key}}`, value),
+      this.template,
+    );
+  }
+}
+
+export default Processor;
