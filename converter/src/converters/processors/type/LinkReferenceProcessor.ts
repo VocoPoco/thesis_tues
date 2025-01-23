518884ccc63d8a65837c87eb3417e905f2d3964b
VocoPoco <vik.penev@gmail.com>
2025-01-24 01:56:03 +0200

    feat: logic for reference based elements added

---
+++ converter/src/converters/processors/type/LinkReferenceProcessor.ts
@@ -0,0 +1,19 @@
+import { RootContent } from 'mdast';
+import ReferenceProcessor from './ReferenceProcessor.js';
+
+class LinkReferenceProcessor extends ReferenceProcessor {
+  protected extractAdditionalProperties(
+    node: RootContent,
+  ): Record<string, string> {
+    const value =
+      'children' in node &&
+      node.children.length > 0 &&
+      'value' in node.children[0]
+        ? (node.children[0] as any).value
+        : '';
+
+    return { value };
+  }
+}
+
+export default LinkReferenceProcessor;
--- converter/src/converters/processors/type/ReferenceProcessor.ts
+++ converter/src/converters/processors/type/ReferenceProcessor.ts
@@ -1,37 +1,41 @@
 import { RootContent } from 'mdast';
 import Processor from './Processor.js';
 
-class LinkReferenceProcessor extends Processor {
-  private _linkReferenceMap: Map<string, string>;
+abstract class ReferenceProcessor extends Processor {
+  protected _referenceMap: Map<string, string>;
+  protected _lineMap: Map<string, number>;
 
-  private _lineMap: Map<string, number>;
+  public get referenceMap(): Map<string, string> {
+    return this._referenceMap;
+  }
 
   public get lineMap(): Map<string, number> {
     return this._lineMap;
   }
 
-  public get linkReferenceMap(): Map<string, string> {
-    return this._linkReferenceMap;
-  }
-
   constructor(template: string) {
     super(template);
-    this._linkReferenceMap = new Map();
+    this._referenceMap = new Map();
     this._lineMap = new Map();
   }
 
   public constructProperties(node: RootContent): Record<string, string> {
-    const identifier = 'identifier' in node ? node.identifier : '';
+    const identifier = 'identifier' in node ? (node as any).identifier : '';
+    const line = node.position?.start.line;
+
     if (identifier) {
-      this._linkReferenceMap.set(identifier, this.template);
-      const line = node.position?.start.line;
+      this._referenceMap.set(identifier, this.template);
       if (line !== undefined) {
-        this.lineMap.set(identifier, line);
+        this._lineMap.set(identifier, line);
       }
-      return { value: `{${identifier}}` };
     }
-    return { value: '' };
+
+    return this.extractAdditionalProperties(node);
   }
+
+  protected abstract extractAdditionalProperties(
+    node: RootContent,
+  ): Record<string, string>;
 }
 
-export default LinkReferenceProcessor;
+export default ReferenceProcessor;
